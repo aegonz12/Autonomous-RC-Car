6a8dedd3c219a00a27a9819abb9c48124f784319
aegonz12 <anthonyegonzalez@gmail.com>
2017-08-23 13:20:58 -0700

    Added testing loops

--- Servos/servo_library_test.py
+++ Servos/servo_library_test.py
@@ -1,12 +1,42 @@
 import time
 from Servo import Servo
 
-servoPin = "P9_14"
-dutyCycle = Servo.MIDDLE_DUTY_CYCLE
 
-minDuty = Servo.MIN_DUTY_CYCLE
-maxDuty = Servo.MAX_DUTY_CYCLE
+
+servoPin = "P9_14"
+dutyCycle = Servo.MIN_DUTY_CYCLE
+increment = 0.1
+
+
 
 servo = Servo()
-
 servo.attach(servoPin, dutyCycle)
+servo.setDutyCycle(dutyCycle)
+
+time.sleep(1)
+
+print servo.getPinStatus(), "\n"
+
+
+
+while dutyCycle < Servo.MAX_DUTY_CYCLE:
+	dutyCycle = dutyCycle + increment
+	servo.setDutyCycle(dutyCycle)
+
+	time.sleep(0.15)
+	print "DUTY CYCLE: ", servo.getDutyCycle()
+
+
+time.sleep(1)
+
+
+while dutyCycle > Servo.MIN_DUTY_CYCLE:
+	dutyCycle = dutyCycle - increment
+	servo.setDutyCycle(dutyCycle)
+
+	time.sleep(0.15)
+	print "DUTY CYCLE: ", servo.getDutyCycle()
+
+
+
+servo.detach()
